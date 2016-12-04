923244f5d35bba2ce901562cce55b51a52f14399
Kitty Chong <Kitty Chong>
2016-12-03 23:53:41 -0500

    First commit for module 1 assignment

---
+++ app.js
@@ -0,0 +1,29 @@
+(function() {
+  'use strict';
+
+  angular.module('Module1App', [])
+    .controller('Module1Controller', ControllerFunction);
+
+    ControllerFunction.$inject = ['$scope'];
+    function ControllerFunction($scope) {
+        $scope.lunchItems = "";
+        $scope.Message = "";
+
+        $scope.CheckIfTooMuch = function() {
+          if ($scope.lunchItems.length == 0) {
+            $scope.Message = "Please enter data first!";
+          } else {
+            $scope.Message = processLunchItems($scope.lunchItems);
+          }
+        };
+
+        function processLunchItems(lunchItems) {
+          var items = lunchItems.split(',');
+          if (items.length > 3) {
+            return "Too much!";
+          } else {
+            return "Enjoy!";
+          }
+        }
+    }
+})();
